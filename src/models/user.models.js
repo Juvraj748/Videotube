e412b2b233268b0b91dff09f19f36880b2f9b0f7
Juvraj748 <juvraj16@gmail.com>
2025-04-28 20:18:11 +0530

    add methods to user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema(
     {
@@ -50,5 +52,39 @@ const userSchema = new Schema(
         timestamps: true // Automatically adds createdAt, updatedAt for each field
     }
 )
+
+userSchema.pre("save", async function(next){ // Use pre because we want to run it before saving
+    // If password is not being saved or updated, we don't need encrypt it
+    if(!this.modified("password")) next() 
+    
+    this.password = await bcrypt.hash(this.password, 10)
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccesstoken = async function(){
+    // short lived access token
+    return jwt.sign({
+        _id: this._id, 
+        email: this.email, 
+        username: this.username,
+        fullname: this.fullname
+    }, 
+    process.env.ACCESS_TOKEN_SECRET, 
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
+}
+
+userSchema.methods.generateRefreshtoken = async function(){
+    // short lived refresh token
+    return jwt.sign({
+        _id: this._id,
+    }, 
+    process.env.REFRESH_TOKEN_SECRET, 
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY})
+}
 
 export const User = mongoose.model("User", userSchema)
