124e85d5f0262dbc8d6349ef448e14f71331c0da
Juvraj748 <juvraj16@gmail.com>
2025-04-30 15:28:23 +0530

    add error middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(cors({
@@ -11,6 +12,7 @@ app.use(cors({
 app.use(express.json({limit: "16kb" })) // standard practice
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 // import routes
 import healthCheckRouter from "./routes/healthCheck.routes.js"
@@ -18,5 +20,13 @@ import healthCheckRouter from "./routes/healthCheck.routes.js"
 // router
 app.use("/api/v1/healthCheck", healthCheckRouter)
 app.use("/api/v1/healthCheck/test", healthCheckRouter)
+
+
+import userRouter from "./routes/users.routes.js"
+import { errorHandler } from "./middlewares/error.middleware.js"
+
+app.use("/api/v1/users", userRouter)
+
+app.use(errorHandler)
 
 export { app }
